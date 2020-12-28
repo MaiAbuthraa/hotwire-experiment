5f3c5bb8d02edaf857002e77c71fe4885319579c
Mai Abuthraa <maiabuthraa@mais-mbp.mynet>
2020-12-28 14:03:26 +0200

    Stream Create & destroy actions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+
+
+  # broadcast_ : we can have these broadcasted for all user
+  # Could be broadcast_append_to or broadcast_prepend_to
+  after_create_commit {broadcast_append_to "posts"}
+  after_update_commit {broadcast_replace_to "posts"}
+  after_destroy_commit {broadcast_remove_to "posts"}
 end
